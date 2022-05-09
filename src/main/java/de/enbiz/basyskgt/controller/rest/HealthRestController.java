b99f52bf5439b8454d358f7b9d647f725e42f0d8
gellrich <gellrich@enbiz.de>
2022-05-09 11:09:11 +0200

    javadoc

--- src/main/java/de/enbiz/basyskgt/controller/rest/HealthRestController.java
+++ src/main/java/de/enbiz/basyskgt/controller/rest/HealthRestController.java
@@ -16,8 +16,15 @@ public class HealthRestController {
     @Autowired
     HealthRepository healthRepository;
 
+    /**
+     * Transmit a new health value for the Ball Screw. Heath value will be forwarded to the AAS on the AAS-Server.
+     *
+     * @param health current health value as a percentage
+     * @return if health value is > 100 or < 0: HTTP_BAD_REQUEST; else: HTTP_OK
+     */
     @PostMapping("/api/health")
     public ResponseEntity<String> updateHealth(@RequestBody short health) {
+        // TODO send value to BaSyx
         if (health < 0 || health > 100) {
             return new ResponseEntity<>("Health value may not be < 0 or > 100", HttpStatus.BAD_REQUEST);
         }
@@ -26,6 +33,11 @@ public class HealthRestController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    /**
+     * Returns latest health value.
+     *
+     * @return {@link HealthEntity} containing the latest health value and the time it has been submitted at
+     */
     @GetMapping("/api/health")
     public ResponseEntity<HealthEntity> getHealth() {
         HealthEntity result = healthRepository.findFirstByOrderByIdDesc();
