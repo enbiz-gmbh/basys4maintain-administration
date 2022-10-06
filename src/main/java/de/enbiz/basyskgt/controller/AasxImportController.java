b5b28163defee55779a8da5b0b832a89e0b94685
gellrich <gellrich@enbiz.de>
2022-10-06 14:31:15 +0200

    implement BaSyx SDK changes

--- src/main/java/de/enbiz/basyskgt/controller/AasxImportController.java
+++ src/main/java/de/enbiz/basyskgt/controller/AasxImportController.java
@@ -1,9 +1,9 @@
 package de.enbiz.basyskgt.controller;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.eclipse.basyx.aas.bundle.AASBundle;
+import org.eclipse.basyx.aas.factory.aasx.AASXToMetamodelConverter;
 import org.eclipse.basyx.aas.metamodel.api.IAssetAdministrationShell;
-import org.eclipse.basyx.components.aas.aasx.AASXPackageManager;
-import org.eclipse.basyx.support.bundle.AASBundle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -20,7 +20,7 @@ public class AasxImportController {
     private static final Logger logger = LoggerFactory.getLogger(AasxImportController.class);
 
     private static Set<AASBundle> getAasBundlesFromFile(String aasxPath) throws IOException, ParserConfigurationException, InvalidFormatException, SAXException {
-        AASXPackageManager packageManager = new AASXPackageManager(aasxPath);
+        AASXToMetamodelConverter packageManager = new AASXToMetamodelConverter(aasxPath);
         Set<AASBundle> aasBundles = packageManager.retrieveAASBundles();
 
         logger.info("retrieved " + aasBundles.size() + " AASBundles from file");
@@ -31,13 +31,9 @@ public class AasxImportController {
      * Provides the AAS for the Ball Screw from the aasx file
      *
      * @return AAS of the ball screw
-     * @throws IOException
-     * @throws ParserConfigurationException
-     * @throws InvalidFormatException
-     * @throws SAXException
      */
     @Bean
-    IAssetAdministrationShell bsAas(AASBundle bsAasBundle) throws IOException, ParserConfigurationException, InvalidFormatException, SAXException {
+    IAssetAdministrationShell bsAas(AASBundle bsAasBundle) {
         return bsAasBundle.getAAS();
     }
 
