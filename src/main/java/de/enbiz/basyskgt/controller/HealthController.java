694edc2ce042817628e1d5f59e8de0256c7d9722
gellrich <gellrich@enbiz.de>
2023-05-18 16:26:55 +0200

    enable sending health value to AAS Server

--- src/main/java/de/enbiz/basyskgt/controller/HealthController.java
+++ src/main/java/de/enbiz/basyskgt/controller/HealthController.java
@@ -3,6 +3,11 @@ package de.enbiz.basyskgt.controller;
 import de.enbiz.basyskgt.configuration.PortConfiguration;
 import de.enbiz.basyskgt.dto.HealthDTO;
 import org.apache.commons.collections4.queue.CircularFifoQueue;
+import org.eclipse.basyx.aas.manager.ConnectedAssetAdministrationShellManager;
+import org.eclipse.basyx.submodel.metamodel.api.ISubmodel;
+import org.eclipse.basyx.submodel.metamodel.api.identifier.IIdentifier;
+import org.eclipse.basyx.submodel.metamodel.api.identifier.IdentifierType;
+import org.eclipse.basyx.submodel.metamodel.map.identifier.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,13 +19,17 @@ import java.util.List;
 @Controller
 public class HealthController {
     public static final int MAX_BUFFER_SIZE = 100;
-    private CircularFifoQueue<HealthDTO> buffer = new CircularFifoQueue<>(MAX_BUFFER_SIZE);
-
+    public static final String LATEST_HEALTH_VALUE = "LatestHealthValue";
+    public static final IIdentifier HEALTH_SUBMODEL_IDENTIFIER = new Identifier(IdentifierType.IRI, "https://example.com/ids/sm/9130_7090_5032_9769");
+    private final ConnectedAssetAdministrationShellManager aasManager;
     private final PortConfiguration portConfiguration;
     private final Logger log = LoggerFactory.getLogger(HealthController.class);
+    private CircularFifoQueue<HealthDTO> buffer = new CircularFifoQueue<>(MAX_BUFFER_SIZE);
+
 
     @Autowired
-    public HealthController(PortConfiguration portConfiguration) {
+    public HealthController(ConnectedAssetAdministrationShellManager aasManager, PortConfiguration portConfiguration) {
+        this.aasManager = aasManager;
         this.portConfiguration = portConfiguration;
     }
 
@@ -35,7 +44,13 @@ public class HealthController {
         if (portConfiguration.getMappedAasIdentifier(portNumber) == null) {
             throw new IllegalArgumentException("There is no device configured for the given port.");
         }
-        buffer.add(new HealthDTO(health, portConfiguration.getMappedAasIdentifier(portNumber)));
+        IIdentifier aasIdentifier = portConfiguration.getMappedAasIdentifier(portNumber);
+
+        log.debug("getting health submodel for port {} and setting {} to {}", portNumber, LATEST_HEALTH_VALUE, health);
+        ISubmodel healthSubmodel = aasManager.retrieveSubmodel(aasIdentifier, HEALTH_SUBMODEL_IDENTIFIER);
+        healthSubmodel.getSubmodelElement(LATEST_HEALTH_VALUE).setValue(health);
+
+        buffer.add(new HealthDTO(health, aasIdentifier));
     }
 
     public HealthDTO getMostRecent() {
